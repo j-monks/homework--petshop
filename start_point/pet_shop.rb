fbe56399cdb36e0210bc8d23330851a2e4c1bc00
James Monks <J.monks23@gmail.com>
2020-06-19 12:31:14 +0100

    add or remove cash remove pass

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -7,5 +7,10 @@ def total_cash(pet_shop_hash)
 end
 
 def add_or_remove_cash(pet_shop_hash, cash)
-   pet_shop_hash[:admin][:total_cash] += cash
+    if (cash > 0)
+    pet_shop_hash[:admin][:total_cash] += cash
+   elsif (cash < 0)
+    pet_shop_hash[:admin][:total_cash] += cash
+   end
 end
+
